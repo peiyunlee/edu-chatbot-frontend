2a407236dae05b0a2a49da1a3f07e9e120715d43
Peiyun Lee <may1092200258@gmail.com>
2023-03-04 23:50:11 +0800

    fix: edit task

--- src/pages/task/EditTask.js
+++ src/pages/task/EditTask.js
@@ -27,17 +27,14 @@ function EditTask() {
         </div>
         <div className='grid gap-2'>
           <span className='font-bold mr-4'>怎麼進行</span>
-          <span className='font-bold text-gray-400 mr-4'>怎麼討論怎麼進行</span>
           <TextArea className='flex-1' rows={3}/>
         </div>
         <div className='grid gap-2'>
           <span className='font-bold mr-4'>繳交日期</span>
-          <span className='font-bold text-gray-400 mr-4'>怎麼討論繳交日期</span>
           <Input className='flex-1' placeholder="04/02" />
         </div>
         <div className='grid gap-2'>
           <span className='font-bold mr-4'>繳交方式</span>
-          <span className='font-bold text-gray-400 mr-4'>怎麼討論繳交方式</span>
           <Input className='flex-1' placeholder="雲端word" />
         </div>
       </div>
