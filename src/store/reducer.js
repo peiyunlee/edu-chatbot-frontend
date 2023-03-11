844007b28168327db3cf87c8fd0768e967946038
Peiyun Lee <may1092200258@gmail.com>
2023-03-12 03:15:36 +0800

    task

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,13 @@
 import { UPDATE_LINE_USER_PROFILE } from "./action";
 
 const initialState = {
-    lineUserProfile: {
-      displayName: "李佩芸",
-      pictureUrl: "https://profile.line-scdn.net/0h1kk-zWtGbkRGVEViOIsQOzYEbS5lJTdWPWUhI3tcN3J_YCkXOWJ1cHNVYiApMykTb2EiKnZWOHJKRxkiWAKScEFkMHN8ZS8Tazclqg",
-      statusMessage: "☀️🏖️😎👋🥲",
-      userId: "U18204365d05108a3841a98aa1514fbb3"
-    }
+    // lineUserProfile: {
+    //   displayName: "李佩芸",
+    //   pictureUrl: "https://profile.line-scdn.net/0h1kk-zWtGbkRGVEViOIsQOzYEbS5lJTdWPWUhI3tcN3J_YCkXOWJ1cHNVYiApMykTb2EiKnZWOHJKRxkiWAKScEFkMHN8ZS8Tazclqg",
+    //   statusMessage: "☀️🏖️😎👋🥲",
+    //   userId: "U18204365d05108a3841a98aa1514fbb3"
+    // }
+    lineUserProfile: null
 };
 
 export function rootReducer(state = initialState, action) {
