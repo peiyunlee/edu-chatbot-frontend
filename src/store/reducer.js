e1b5d11dbeeaf368ad13de7299b9265d796a8a18
Peiyun Lee <may1092200258@gmail.com>
2023-03-12 11:30:48 +0800

    test

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,13 +1,13 @@
 import { UPDATE_LINE_USER_PROFILE, UPDATE_TASK_LIST }from "./action";
 
 const initialState = {
-    lineUserProfile: {
-      displayName: "李佩芸",
-      pictureUrl: "https://profile.line-scdn.net/0h1kk-zWtGbkRGVEViOIsQOzYEbS5lJTdWPWUhI3tcN3J_YCkXOWJ1cHNVYiApMykTb2EiKnZWOHJKRxkiWAKScEFkMHN8ZS8Tazclqg",
-      statusMessage: "☀️🏖️😎👋🥲",
-      userId: "U18204365d05108a3841a98aa1514fbb3"
-    },
-    // lineUserProfile: null,
+    // lineUserProfile: {
+    //   displayName: "李佩芸",
+    //   pictureUrl: "https://profile.line-scdn.net/0h1kk-zWtGbkRGVEViOIsQOzYEbS5lJTdWPWUhI3tcN3J_YCkXOWJ1cHNVYiApMykTb2EiKnZWOHJKRxkiWAKScEFkMHN8ZS8Tazclqg",
+    //   statusMessage: "☀️🏖️😎👋🥲",
+    //   userId: "U18204365d05108a3841a98aa1514fbb3"
+    // },
+    lineUserProfile: null,
     taskList: [{
       student_id: null,
       tasks:[],
