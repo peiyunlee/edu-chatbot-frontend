6d05b06b92d0574363947c3176c01d2eeecd4aaa
Peiyun Lee <may1092200258@gmail.com>
2023-03-15 21:20:24 +0800

    test: login

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,20 +1,20 @@
 import { UPDATE_USER_PROFILE, UPDATE_TASK_LIST }from "./action";
 
 const initialState = {
-    // userProfile: {
-    //   displayName: "李佩芸",
-    //   pictureUrl: "https://profile.line-scdn.net/0h1kk-zWtGbkRGVEViOIsQOzYEbS5lJTdWPWUhI3tcN3J_YCkXOWJ1cHNVYiApMykTb2EiKnZWOHJKRxkiWAKScEFkMHN8ZS8Tazclqg",
-    //   statusMessage: "☀️🏖️😎👋🥲",
-    //   userId: "U18204365d05108a3841a98aa1514fbb3",
-    //   student: {
-    //     _id: "640c9ac11b96fa848b35f23b",
-    //     group_id: "640c9b221b96fa848b35f23c",
-    //     line_user_id: "U18204365d05108a3841a98aa1514fbb3",
-    //     student_number: "111034016",
-    //     name: "李佩芸"
-    //   }
-    // },
-    userProfile: null,
+    userProfile: {
+      displayName: "李佩芸",
+      pictureUrl: "https://profile.line-scdn.net/0h1kk-zWtGbkRGVEViOIsQOzYEbS5lJTdWPWUhI3tcN3J_YCkXOWJ1cHNVYiApMykTb2EiKnZWOHJKRxkiWAKScEFkMHN8ZS8Tazclqg",
+      statusMessage: "☀️🏖️😎👋🥲",
+      userId: "U18204365d05108a3841a98aa1514fbb3",
+      student: {
+        _id: "640c9ac11b96fa848b35f23b",
+        group_id: "640c9b221b96fa848b35f23c",
+        line_user_id: "U18204365d05108a3841a98aa1514fbb3",
+        student_number: "111034016",
+        name: "李佩芸"
+      }
+    },
+    // userProfile: null,
     taskList: [{
       student_id: null,
       tasks:[],
